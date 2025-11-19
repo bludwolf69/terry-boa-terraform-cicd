913517c6fe7c5d29c57045f942198fdba6640a6b
bludwolf69 <bludwolf@bellsouth.net>
2025-11-19 14:28:22 +0000

    Updates to terraform project

---
+++ ec2-tf-data/securityGroups.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "ashu_sec-groups" {
+  name        = "Terry_allow_rules"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.ashu-exiting-vpc.id
+
+  tags = {
+    Name = "Terry_firewall_rules"
+  }
+}
+/*
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.ashu_sec-groups.id
+  cidr_ipv4         = "34.229.87.158"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+*/
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.ashu_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+data "aws_vpc" "ashu-exiting-vpc"  {
+  id = "vpc-07eede624774fec02"
+  # above vpc id is already existing
+}
