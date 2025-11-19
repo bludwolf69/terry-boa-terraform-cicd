0eb4b43a1ee021124b91280a03139a08e7b2faf3
bludwolf69 <bludwolf@bellsouth.net>
2025-11-19 15:12:22 +0000

    new security rule for terry rules

--- ec2-tf-data/securityGroups.tf
+++ ec2-tf-data/securityGroups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ashu_sec-groups" {
+resource "aws_security_group" "terry_sec-groups" {
   name        = "Terry_allow_rules"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = data.aws_vpc.ashu-exiting-vpc.id
@@ -9,7 +9,7 @@ resource "aws_security_group" "ashu_sec-groups" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.ashu_sec-groups.id
+  security_group_id = aws_security_group.terry_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
-  security_group_id = aws_security_group.ashu_sec-groups.id
+  security_group_id = aws_security_group.terry_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
@@ -25,7 +25,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.ashu_sec-groups.id
+  security_group_id = aws_security_group.terry_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
