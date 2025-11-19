764ec99010fd8326c0445a6c5c222513fec8dfcf
bludwolf69 <bludwolf@bellsouth.net>
2025-11-19 14:59:05 +0000

    Updated Security rules

--- ec2-tf-data/securityGroups.tf
+++ ec2-tf-data/securityGroups.tf
@@ -7,15 +7,23 @@ resource "aws_security_group" "ashu_sec-groups" {
     Name = "Terry_firewall_rules"
   }
 }
-/*
+
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.ashu_sec-groups.id
-  cidr_ipv4         = "34.229.87.158"
-  from_port         = 443
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 22
 }
-*/
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
+  security_group_id = aws_security_group.ashu_sec-groups.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.ashu_sec-groups.id
   cidr_ipv4         = "0.0.0.0/0"
